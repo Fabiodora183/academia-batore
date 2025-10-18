4072acd8603c0540446ed7fd3aa15e01c77d76c6
Fabio dorakiewicz alves <fabinho.dorakiewicz@gmail.com>
2025-10-18 09:45:47 -0300

    add navbar com titulo e cor de fundo clara

--- PHP/index.php
+++ PHP/index.php
@@ -5,38 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AcademiaBatoré</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <link href="https://cdn.tailwindcss.com" rel="stylesheet">
   </head>
   <body>
-    <!-- Barra de navegação, usando bootstrap -->
-<ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="login.php">Login/Cadastro</a>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Itens</a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="suplementos.php">Loja de suplementos</a></li>
-      <li><a class="dropdown-item" href="planos.php">Planos</a></li>
-      <li><a class="dropdown-item" href="nutricionista.php">Nutricionista</a></li>
-      <li><a class="dropdown-item" href="personal.php">Personal</a></li>
-      <li><hr class="dropdown-divider"></li>
-      <li><a class="dropdown-item" href="ajuda.php">Ajuda</a></li>
-    </ul>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="parceiros.php">Parceiros</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
-    <!-- Barra de navegação, usando bootstrap -->
 
-  <main>
-
-    <h1>Bem vindo à Academia Batoré</h1>
-
-  </main>
+<!-- Barra de navegação, usando bootstrap -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand fs-1">Academia Batoré</a> <!-- Título grande à esquerda -->
+  <div class="navbar-nav ms-auto"> <!-- Itens à direita -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Itens</a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="suplementos.php">Loja de suplementos</a></li>
+        <li><a class="dropdown-item" href="planos.php">Planos</a></li>
+        <li><a class="dropdown-item" href="nutricionista.php">Nutricionista</a></li>
+        <li><a class="dropdown-item" href="personal.php">Personal</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="ajuda.php">Ajuda</a></li>
+      </ul>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="parceiros.php">Parceiros</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="login.php">Login/Cadastro</a>
+    </li>
+  </div>
+</nav>
+<!-- Barra de navegação, usando bootstrap -->
 
 
 
@@ -44,6 +43,7 @@
 
 
 
+<script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
