33b000f697348ea302af34c0e33209b0c4eb1174
Fabio dorakiewicz alves <fabinho.dorakiewicz@gmail.com>
2025-10-17 22:34:47 -0300

    ++navbar
    adição da navbar.

--- PHP/index.php
+++ PHP/index.php
@@ -7,11 +7,30 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-<nav>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-</nav>
+    <!-- Barra de navegação, usando bootstrap -->
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="login.php">Login/Cadastro</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Itens</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="suplementos.php">Loja de suplementos</a></li>
+      <li><a class="dropdown-item" href="planos.php">Planos</a></li>
+      <li><a class="dropdown-item" href="nutricionista.php">Nutricionista</a></li>
+      <li><a class="dropdown-item" href="personal.php">Personal</a></li>
+      <li><hr class="dropdown-divider"></li>
+      <li><a class="dropdown-item" href="ajuda.php">Ajuda</a></li>
+    </ul>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="parceiros.php">Parceiros</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+    <!-- Barra de navegação, usando bootstrap -->
 
   <main>
 
